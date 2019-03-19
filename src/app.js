4c59cb773c1439e84ec9b1615319b143016dd8b1
BlackWizarz <dngrowth01@gmail.com>
2019-03-19 17:55:02 +0700

    Create a Component For Note-App

--- src/app.js
+++ src/app.js
@@ -1,6 +1,62 @@
-console.log('Hello App')
-
-let Template = <p>This is JSX</p>
 
 
-ReactDOM.render(<Template />, document.querySelector('#app'))
+class Header extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Note-App</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
+    }
+}
+
+
+class Action extends React.Component {
+    render () {
+        return (
+            <div>
+                <button>What Should I do?</button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render () {
+        return (
+            <div>
+            Options
+            <Option />
+            <Option />
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component { 
+    render () {
+        return (
+            <div>Option</div>
+        )
+    }
+}
+
+class AddOption extends React.Component { 
+    render () {
+        return (
+            <div>Add Options</div>
+        )
+    }
+}
+
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+)
+
+ReactDOM.render(jsx, document.querySelector('#app'))
